a6acf6d9db7e4c61c760ea9c8f4f96a5ce7df947
Sieder09 <thomassieder91@gmail.com>
2024-09-26 16:27:38 +0200

    Initial commit

---
+++ js.js
@@ -0,0 +1,15 @@
+
+
+function checkeingabe() {
+    const mensa = documents.getElementByID('mensa');
+const mensaValue = mensa.value;
+
+if (mensa == "Help"){
+    console.log("hello wordl!");
+}
+
+else 
+{
+    console.log("siggidy sieder!");
+}
+}
